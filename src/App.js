af4a4e0164e5d74e657f8338657d08e641f55fba
BonesTheGhost <bonestheghost@gmail.com>
2020-01-15 14:57:14 -0600

    [2] BASIC ROUTES:: Imported the 'switch' component and enforced routes in a more useful way.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
-//When importing we can use 'as' to import a component under an alias to make our lives easier. 'Router' helps with our routes, 'Route' is the JSX component we need for specific routes, and 'Redirect' is for enforcing which routes user has access to.
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+//When importing we can use 'as' to import a component under an alias to make our lives easier. 'Router' helps with our routes, 'Route' is the JSX component we need for specific routes, and 'Redirect' is for enforcing which routes user has access to. 'Switch' component can be wrapped around your route definitions to manage route logic i.e. 'redirect'.
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
 
 
 //<Route> is a JSX component that we can use when importing 'react-router-dom'. It allows for a 'path' argument, and then whatever needs to be displayed is between the opening and closing tags.
 const App = () => {
   //We wrap 'Router' around everything that should be able to use our router. Ex: pages.
   return <Router>
+    <Switch>
     <Route path="/" exact={true}>
       <Users />
     </Route>
+
+    <Route path="/places/new" exact={true}>
+      <NewPlace />
+    </Route>
+
     <Redirect to='/' />
+    </Switch>
+    
   </Router>;
 }
 
