d765776784b479e44d8626a48a260eb57d6c94b7
BonesTheGhost <bonestheghost@gmail.com>
2020-01-15 21:20:28 -0600

    [6] USER PLACES:: Added the ability to filter places based on user Id. These can be accessed via the URL right now. Will refine.

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,22 +1,22 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 import PlaceList from '../components/PlaceList';
 
-const UserPlaces = () => {
-   const DUMMY_PLACES = [
-       {
-           id: 'p1',
-           title: 'Empire State Building',
-           description: 'One of the talles buildings in the world',
-           imageURL: 'https://en.wikipedia.org/wiki/Empire_State_Building#/media/File:Empire_State_Building_(aerial_view).jpg',
-           address: '20 W 34th St, New York, NY 10001',
-           location: {
-               lat: 40.7484405,
-               lng: -73.9878584
-           },
-           creator: 'u1'
-       },
-       {
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the talles buildings in the world',
+        imageURL: 'https://en.wikipedia.org/wiki/Empire_State_Building#/media/File:Empire_State_Building_(aerial_view).jpg',
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878584
+        },
+        creator: 'u1'
+    },
+    {
         id: 'p2',
         title: 'Empire State Building',
         description: 'One of the talles buildings in the world',
@@ -28,9 +28,15 @@ const UserPlaces = () => {
         },
         creator: 'u2'
     }
-   ] 
-    
-    return <PlaceList items={DUMMY_PLACES} />
+];
+
+const UserPlaces = () => {
+    const userId = useParams().userId;
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+    return <PlaceList items={loadedPlaces} />
 };
+
+
+
 
 export default UserPlaces;
